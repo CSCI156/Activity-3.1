1a4d4dc433779ec394308e18ae7d86e659193d45
theweirdowombat <kaitlynmariecookis@gmail.com>
2021-01-02 01:30:08 -0500

    Added procedure called "line"

--- Activity 3.1.py
+++ Activity 3.1.py
@@ -5,3 +5,13 @@ def right_justify(s):
 
 right_justify("Allen")
 right_justify("Python is amazing!")
+
+
+def line(x):
+    x = "+"+("-"*((x-3)/2))+"+"+("-"*((x-3)/2))+"+"
+    print(x)
+
+
+line(5)
+line(9)
+line(25)
